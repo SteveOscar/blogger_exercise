0e88249fbc43204d84ddff4ce7c3c66a0c8d0b58
Steven Olson <steveoscaro@gmail.com>
2015-12-16 15:04:12 -0700

    finished iteration 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,45 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.create(article_params)
+
+    flash.notice = "Article '#{@article.title}' was created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' was deleted!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(article_params)
+
+    flash.notice = "Article '#{@article.title}' was updated!"
+
+    redirect_to articles_path
+  end
+
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
